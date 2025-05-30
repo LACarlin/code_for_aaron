dd5d0847d804189728ef07dc1e4229d2e7c711a8
LACarlin <linzicarlin@gmail.com>
2025-05-30 16:39:27 +0100

    add part 2

---
+++ javascript/1_types_and_operators/2_arrays_and_objects/main.js
@@ -0,0 +1,11 @@
+// This is an empty array
+const emptyArray = []
+
+// This is an array of numbers
+let position = [0, 1]
+
+// This is an array of strings
+const myFavouriteFruit = ["banana", 'apple', `kiwi`]
+
+// This is an array of a bunch of different things
+const mixedBag = ["banana", 12, false]
