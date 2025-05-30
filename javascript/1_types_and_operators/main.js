97c8c4b0e7d66151fe44385f21336c23666e1fe5
Linzi Carlin <linzicarlin@NIMBLE-00207.local>
2025-05-30 15:45:11 +0100

    add original files

---
+++ javascript/1_types_and_operators/main.js
@@ -0,0 +1,38 @@
+// console.log('Hello World'); // string
+// console.log("Hello World");
+// console.log(`Hello World`);
+
+// console.log(1);
+// console.log("1");
+
+// console.log(true);
+// console.log(false);
+
+// console.log("Hello World" + " and then goodbye");
+// console.log(1 + 2);
+
+// const myName = "Aaron";
+// console.log("Hello " + myName);
+
+// const myAge = 12;
+// console.log("I am " + myAge + " years old");
+// console.log("Hello, my name is " + myName + " and I am " + myAge + " years old");
+
+// console.log(`Hello, my name is ${myName} and I am ${myAge} years old`);
+
+// const yourAge = "47";
+// let yourAge = "47";
+// console.log(myAge + yourAge);
+
+// yourAge = 47;
+// console.log(myAge + yourAge);
+
+// console.log("hello" + true);
+
+// console.log(myAge === 12);
+
+let a = 5
+let b = a + 1
+a = 7
+
+console.log(b)
